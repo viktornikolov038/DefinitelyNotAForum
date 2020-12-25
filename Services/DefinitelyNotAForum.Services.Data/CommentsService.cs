f8fd2ad9da90c5f6f2dfd060588deadc388b7491
viktor <viktor.nikolov038@gmail.com>
2020-12-25 16:10:09 +0200

    Comments
    Signed-off-by: viktor <viktor.nikolov038@gmail.com>

---
+++ Services/DefinitelyNotAForum.Services.Data/CommentsService.cs
@@ -0,0 +1,41 @@
+﻿using DefinitelyNotAForum.Data.Common.Repositories;
+using DefinitelyNotAForum.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefinitelyNotAForum.Services.Data
+{
+    public class CommentsService : ICommentsService
+    {
+        private readonly IDeletableEntityRepository<Comment> commentsRepository;
+
+        public CommentsService(
+            IDeletableEntityRepository<Comment> commentsRepository)
+        {
+            this.commentsRepository = commentsRepository;
+        }
+
+        public async Task Create(int postId, string userId, string content, int? parentId = null)
+        {
+            var comment = new Comment
+            {
+                Content = content,
+                ParentId = parentId,
+                PostId = postId,
+                UserId = userId,
+            };
+            await this.commentsRepository.AddAsync(comment);
+            await this.commentsRepository.SaveChangesAsync();
+        }
+
+        public bool IsInPostId(int commentId, int postId)
+        {
+            var commentPostId = this.commentsRepository.All().Where(x => x.Id == commentId)
+                .Select(x => x.PostId).FirstOrDefault();
+            return commentPostId == postId;
+        }
+    }
+}
