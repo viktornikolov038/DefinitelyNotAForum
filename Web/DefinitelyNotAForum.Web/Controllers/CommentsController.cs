b71122375a8d747911f387f8a3d10f9d93ba5723
viktor <viktor.nikolov038@gmail.com>
2020-12-28 04:22:54 +0200

    missing files

---
+++ Web/DefinitelyNotAForum.Web/Controllers/CommentsController.cs
@@ -0,0 +1,47 @@
+﻿namespace DefinitelyNotAForum.Web.Controllers
+{
+    using System.Threading.Tasks;
+
+    using DefinitelyNotAForum.Data.Models;
+    using DefinitelyNotAForum.Services.Data;
+    using DefinitelyNotAForum.Web.ViewModels.Comments;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class CommentsController : Controller
+    {
+        private readonly ICommentsService commentsService;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public CommentsController(
+            ICommentsService commentsService,
+            UserManager<ApplicationUser> userManager)
+        {
+            this.commentsService = commentsService;
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Create(CreateCommentInputModel input)
+        {
+            var parentId =
+                input.ParentId == 0 ?
+                    (int?)null :
+                    input.ParentId;
+
+            if (parentId.HasValue)
+            {
+                if (!this.commentsService.IsInPostId(parentId.Value, input.PostId))
+                {
+                    return this.BadRequest();
+                }
+            }
+
+            var userId = this.userManager.GetUserId(this.User);
+            await this.commentsService.Create(input.PostId, userId, input.Content, parentId);
+            return this.RedirectToAction("ById", "Posts", new { id = input.PostId });
+        }
+    }
+}
