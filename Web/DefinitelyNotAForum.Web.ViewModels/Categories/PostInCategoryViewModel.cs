2ce409fd0458dd69ff2cf5fb80668706b08c5bed
viktor <viktor.nikolov038@gmail.com>
2020-12-28 04:25:20 +0200

    missing files

---
+++ Web/DefinitelyNotAForum.Web.ViewModels/Categories/PostInCategoryViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace DefinitelyNotAForum.Web.ViewModels.Categories
+{
+    using System;
+    using System.Net;
+    using System.Text.RegularExpressions;
+    using DefinitelyNotAForum.Data.Models;
+    using DefinitelyNotAForum.Services.Mapping;
+
+    public class PostInCategoryViewModel : IMapFrom<Post>
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string ShortContent
+        {
+            get
+            {
+                var content = WebUtility.HtmlDecode(Regex.Replace(this.Content, @"<[^>]+>", string.Empty));
+                return content.Length > 80
+                        ? content.Substring(0, 80) + "..."
+                        : content;
+            }
+        }
+
+        public string UserUserName { get; set; }
+
+        public int CommentsCount { get; set; }
+    }
+}
